c8fd556bd9a5600937a53718ea9031a0c37ddc40
a-voinov <alariht@gmail.com>
2019-02-06 20:51:23 +0700

    Pagination and custom path configuration

--- src/main/java/com/tutorial/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/tutorial/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.tutorial.springboot.cruddemo.dao;
 
 import com.tutorial.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //that's it...
 }
